5e8bb0fb60fd50d91c16b7d8fc07f0a87ea2c5a3
unknown <synergy2411@outlook.com>
2020-03-02 13:10:27 +0530

    lib utilized

--- projects/users-app/src/app/app.component.ts
+++ projects/users-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-app';
+
+
+  fromChild(defaultTempl : TemplateRef<any>){
+    console.log("IN APP COMP : ", defaultTempl);
+  }
 }
--- projects/users-app/src/app/app.module.ts
+++ projects/users-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { GepUtilLibModule } from 'my-gep-util';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -14,7 +15,7 @@ import { HightlightDirective } from './directives/highlight.directive';
     HightlightDirective
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, GepUtilLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
